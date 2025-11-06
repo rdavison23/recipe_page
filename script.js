8b7cb773298ab0a9d93dcd21ac972d6c38e4dd4c
Your Name <your-email@example.com>
2025-11-06 08:32:34 -0800

    Adding a strikethrough when someone clicks an element

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ let myList = document.getElementById("ingredientsList")
 let label= document.createElement("label");
 label.htmlfor ="id";
 label.appendChild(
-    document.createTextNode("This creates the lable for checkbox"));
+    document.createTextNode(""));
 // myDiv.appendChild(checkbox);
 // myDiv.appendChild(checkbox);
 myDiv.appendChild(label);
 
-for (const child of myList.children) {
-    console.log(child)
+for (const [i,child] of Array.from(myList.children).entries()) {
+    console.log(i,child)
     let checkbox = document.createElement("input")
     checkbox.type = "checkbox"
-    checkbox.name="name";
-    checkbox.value ="value";
-    checkbox.id ="id";
+    checkbox.name="ingredients";
+    checkbox.id ="ingredient";
     checkbox.classList.add("bigCheckbox")
     child.prepend(checkbox)
+    
 }
