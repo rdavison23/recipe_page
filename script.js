ea85f1303fbd4e0991f1d2cf5311455d45037612
Your Name <your-email@example.com>
2025-11-05 18:26:35 -0800

    Add dynamic checkbox generation for ingredient list with label creation

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-document.querySelectorAll('ul li')
+let myDiv = document.getElementById("ingredients");
+let myList = document.getElementById("ingredientsList")
+
+let label= document.createElement("label");
+label.htmlfor ="id";
+label.appendChild(
+    document.createTextNode("This create the lable for checkbox"));
+// myDiv.appendChild(checkbox);
+// myDiv.appendChild(checkbox);
+myDiv.appendChild(label);
+
+for (const child of myList.children) {
+    console.log(child)
+    let checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    checkbox.name="name";
+    checkbox.value ="value";
+    checkbox.id ="id";
+    checkbox.classList.add("bigCheckbox")
+    child.prepend(checkbox)
+}
